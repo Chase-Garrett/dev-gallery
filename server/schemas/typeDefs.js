1846bc97cef09532e30540e55da1eb0c9b8034af
Chase <chasegarrett@tutanota.com>
2023-11-20 17:55:49 -0600

    typeDefs for models and API

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,13 +17,24 @@ const typeDefs = `
 
   type Query {
     user: User
+    users: [User]
   }
 
   type Mutation {
-    signin(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    signup(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     addSkills(skillName: String!): User
     removeSkill(skillName: String!): User
+    addProject(projectName: String!, projectDescription: String!, projectUrl: String!, projectRepo: String!, projectScreenshot: String!, projectSkills: [ID]!): User
+    removeProject(projectId: ID!): User
+    addProjectSkill(projectId: ID!, skillId: ID!): User
+    removeProjectSkill(projectId: ID!, skillId: ID!): User
+    addBio(bio: String!): User
+    updateBio(bio: String!): User
+    addProfilePic(profilePic: String!): User
+    removeProfilePic: User
+    addProfile(firstName: String!, lastName: String!, email: String!, bio: String!, isDev: Boolean!): User
+    removeProfile: User
   }
 `;
 
