6784ce3490c4d5a105fc13b98a887eb495ece852
dan-watkins <39423254+dan-watkins@users.noreply.github.com>
2023-11-20 19:52:53 -0600

    added isDeveloper checkbox to use mutation and updated models for projects and skills

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -36,11 +36,11 @@ const typeDefs = `
   }
 
   type Mutation {
-    signup(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    signup(firstName: String!, lastName: String!, email: String!, password: String!, isDev: Boolean!): Auth
     login(email: String!, password: String!): Auth
     addSkills(skillName: String!): User
     removeSkill(skillName: String!): User
-    addProject(projectName: String!, projectDescription: String!, projectUrl: String!, projectRepo: String!, projectScreenshot: String!, projectSkills: [ID]!): User
+    addProject(projectName: String!, projectDescription: String!, projectUrl: String, projectRepo: String!, projectScreenshot: String, projectSkills: [ID]!): User
     removeProject(projectId: ID!): User
     addProjectSkill(projectId: ID!, skillId: ID!): User
     removeProjectSkill(projectId: ID!, skillId: ID!): User
