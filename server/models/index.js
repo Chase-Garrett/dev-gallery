4163aa26e5676289056ca6cfa7cfbe431e240713
Chase <chasegarrett@tutanota.com>
2023-11-27 15:59:33 -0600

    add message and thread to index

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const User = require("./User");
 const Skill = require("./Skill");
 const Project = require("./Project");
+const Message = require("./Message");
+const Thread = require("./Thread");
 
-module.exports = { User, Project, Skill };
+module.exports = { User, Project, Skill, Message, Thread };
