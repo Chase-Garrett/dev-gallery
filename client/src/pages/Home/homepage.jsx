f9f486efae7f60eead68506ab77516facb00b895
Watkins <Dan.Watkins@coxautoinc.com>
2023-11-27 16:32:48 -0500

    set up homepage for searching, adding button for search bar

--- client/src/pages/Home/homepage.jsx
+++ client/src/pages/Home/homepage.jsx
@@ -1,15 +1,36 @@
 import Dashboard from "../../components/Dashboard";
 import { ProjectCard } from "../../components/Card";
-import SearchBar from "../../components/Search";
+import { Box, TextField, Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ALL_USERS, QUERY_USER } from "../../utils/queries";
 
 export default function Homepage () {
+  const { loading, data } = useQuery(QUERY_USER)
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+  }
+
 return (
   <>
   <div className="dashboard">
     <Dashboard />
   </div>
   <div className="projectSearch">
-    <SearchBar />
+  <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="Developers" variant="standard" />
+      <Button variant='outlined' sx={{width: 5}} onClick={handleFormSubmit}>Search</Button>
+    </Box>
   </div>
   <div className="projectCard">
     <ProjectCard />
